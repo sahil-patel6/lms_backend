06679ac12bfc18eed5cca1dc4245a5313cfa8b06
sahil-patel6 <goal.sahil6@gmail.com>
2023-03-05 18:40:56 +0530

    changes in file_schema

--- firebase_admin.js
+++ firebase_admin.js
@@ -7,4 +7,5 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
+
 module.exports = admin;
--- models/file_schema.js
+++ models/file_schema.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
+  name_of_file:{
+    type: String,
+    required: true,
+  },
   download_url: {
     type: String,
     required: true,
