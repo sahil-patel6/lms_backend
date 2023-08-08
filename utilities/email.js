81a1f71a794d49762921907216ad5d8a1b2be5a5
sahil-patel6 <goal.sahil6@gmail.com>
2023-08-08 17:37:46 +0530

    changes in email

--- utilities/email.js
+++ utilities/email.js
@@ -1,40 +1,16 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
 
 const createTransporter = async () => {
-  const oauth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
-
-  oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-  });
-
-  const accessToken = await new Promise((resolve, reject) => {
-    oauth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject();
-      }
-      resolve(token);
-    });
-  });
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
+    service: 'gmail',
+    auth:{
       user: process.env.EMAIL,
-      accessToken,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
-    },
-  });
-
+      pass: process.env.EMAIL_APP_PASSWORD
+    }
+  })
+  
   return transporter;
 };
 
